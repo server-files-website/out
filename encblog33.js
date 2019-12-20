dedf0d6f2c57b31e5cbc78dec028500ee104a6e7
server-files-website <56220522+server-files-website@users.noreply.github.com>
2019-12-20 02:42:35 -0800

    Create encblog33.js

---
+++ encblog33.js
@@ -0,0 +1,46 @@
+var _0xe479 = [ "screen and (min-width: 60em)", "matchMedia", "matches", "onload", "credit", "getElementById", "href", "location", "https://www.naminakiky.com", "setAttribute", "rel", "nofollow", "innerHTML", "Template by <a href='https://www.naminakiky.com' rel='nofollow noopener' target='_blank' title='Namina Kiky'>Namina Kiky</a>", "click", "preventDefault", "open", "addClass", "#searchfg", "focus", '#searchfg > form > input[type="search"]', "on", 'a[href="#searchfg"]', "click keyup", "target", "close", "className", "keyCode", "removeClass", "#searchfg, #searchfg button.close", "menumaker", "fn", "dropdown", "extend", "menu-opened", "toggleClass", "ul", "next", "hasClass", "slideToggle", "format", "show", "find", ".button", "has-sub", "parent", "li ul", '<span class="submenu-button"></span>', "prepend", ".has-sub", "submenu-opened", "siblings", ".submenu-button", "multitoggle", "sticky", "position", "fixed", "css", "each", "#freemenu", "ready" ];
+
+var mql = window[_0xe479[1]](_0xe479[0]);
+
+if (mql[_0xe479[2]]) window[_0xe479[3]] = function() {
+    var a = document[_0xe479[5]](_0xe479[4]);
+    if (null == a) window[_0xe479[7]][_0xe479[6]] = _0xe479[8];
+    a[_0xe479[9]](_0xe479[6], _0xe479[8]);
+    a[_0xe479[9]](_0xe479[10], _0xe479[11]);
+    a[_0xe479[12]] = _0xe479[13];
+};
+
+$(function() {
+    $(_0xe479[22])[_0xe479[21]](_0xe479[14], function(a) {
+        a[_0xe479[15]](), $(_0xe479[18])[_0xe479[17]](_0xe479[16]), $(_0xe479[20])[_0xe479[19]]();
+    }), $(_0xe479[29])[_0xe479[21]](_0xe479[23], function(a) {
+        a[_0xe479[24]] != this && _0xe479[25] != a[_0xe479[24]][_0xe479[26]] && 27 != a[_0xe479[27]] || $(this)[_0xe479[28]](_0xe479[16]);
+    });
+});
+
+!function(a) {
+    a[_0xe479[31]][_0xe479[30]] = function(b) {
+        var c = a(this), d = a[_0xe479[33]]({
+            format: _0xe479[32],
+            sticky: !1
+        }, b);
+        return this[_0xe479[58]](function() {
+            a(this)[_0xe479[42]](_0xe479[43])[_0xe479[21]](_0xe479[14], function() {
+                a(this)[_0xe479[35]](_0xe479[34]);
+                var b = a(this)[_0xe479[37]](_0xe479[36]);
+                b[_0xe479[38]](_0xe479[16]) ? b[_0xe479[39]](150)[_0xe479[28]](_0xe479[16]) : (b[_0xe479[39]](150)[_0xe479[17]](_0xe479[16]), 
+                _0xe479[32] === d[_0xe479[40]] && b[_0xe479[42]](_0xe479[36])[_0xe479[41]]());
+            }), c[_0xe479[42]](_0xe479[46])[_0xe479[45]]()[_0xe479[17]](_0xe479[44]), multiTg = function() {
+                c[_0xe479[42]](_0xe479[49])[_0xe479[48]](_0xe479[47]), c[_0xe479[42]](_0xe479[52])[_0xe479[21]](_0xe479[14], function() {
+                    a(this)[_0xe479[35]](_0xe479[50]), a(this)[_0xe479[51]](_0xe479[36])[_0xe479[38]](_0xe479[16]) ? a(this)[_0xe479[51]](_0xe479[36])[_0xe479[28]](_0xe479[16])[_0xe479[39]](150) : a(this)[_0xe479[51]](_0xe479[36])[_0xe479[17]](_0xe479[16])[_0xe479[39]](150);
+                });
+            }, _0xe479[53] === d[_0xe479[40]] ? multiTg() : c[_0xe479[17]](_0xe479[32]), !0 === d[_0xe479[54]] && c[_0xe479[57]](_0xe479[55], _0xe479[56]);
+        });
+    };
+}(jQuery), function(a) {
+    a(document)[_0xe479[60]](function() {
+        a(_0xe479[59])[_0xe479[30]]({
+            format: _0xe479[53]
+        });
+    });
+}(jQuery);
