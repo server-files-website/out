dd9ae7602e69c0ce82ec246f1598b1e066c63e39
server-files-website <56220522+server-files-website@users.noreply.github.com>
2019-12-20 02:35:54 -0800

    Create encblog1.js

---
+++ encblog1.js
@@ -0,0 +1,168 @@
+var _0x4aeb = [ "screen and (min-width: 60em)", "matchMedia", "matches", "onload", "credit", "getElementById", "href", "location", "https://www.naminakiky.com", "setAttribute", "rel", "nofollow", "innerHTML", "Template by <a href='https://www.naminakiky.com' rel='nofollow noopener' target='_blank' title='Namina Kiky'>Namina Kiky</a>", "use strict", "now", "ias", "defaults", "extend", "util", "scrollContainer", "paging", "history", "scroll", "getCurrentScrollOffset", "getCurPageNum", "triggerPageThreshold", "after", "last", "item", "find", "container", "fadeIn", "unbind", "hide", "pagination", "size", "top", "offset", "height", "thresholdMargin", "attr", "next", "noneleft", "beforePageChange", "isFunction", "pushPages", "customLoaderProc", "call", "onLoadItems", "replaceWith", "remove", "onRenderComplete", "loaderDelay", "length", "eq", "filter", "push", "each", "html", "get", ".ias_loader", '<div class="ias_loader" style="text-align:center;">', "loader", "</div>", ".ias_trigger", '<div class="ias_trigger"><a href="#">', "trigger", "</a></div>", "click", "on", "off", "a", "setPage", "onPageChange", "onChangePage", "havePage", "getPage", "swing", "animate", "html,body", "scrollTop", "html, body", "forceScrollTop", ".blog-posts", ".post-grid", "#blog-pager", "#blog-pager-older-link a", '<span class="loader-page">Loading</span>', "Load More", "load", "pushState", "replaceState", "", "updateState", "getState", "page", "state", "#/page/", "substring", "hash", "replace" ];
+
+var mql = window[_0x4aeb[1]](_0x4aeb[0]);
+
+if (mql[_0x4aeb[2]]) window[_0x4aeb[3]] = function() {
+    var a = document[_0x4aeb[5]](_0x4aeb[4]);
+    if (null == a) window[_0x4aeb[7]][_0x4aeb[6]] = _0x4aeb[8];
+    a[_0x4aeb[9]](_0x4aeb[6], _0x4aeb[8]);
+    a[_0x4aeb[9]](_0x4aeb[10], _0x4aeb[11]);
+    a[_0x4aeb[12]] = _0x4aeb[13];
+};
+
+!function(a) {
+    _0x4aeb[14];
+    Date[_0x4aeb[15]] = Date[_0x4aeb[15]] || function() {
+        return +new Date();
+    }, a[_0x4aeb[16]] = function(b) {
+        var c, d = a[_0x4aeb[18]]({}, a[_0x4aeb[16]][_0x4aeb[17]], b), e = new a[_0x4aeb[16]][_0x4aeb[19]](), f = new a[_0x4aeb[16]][_0x4aeb[21]](d[_0x4aeb[20]]), g = !!d[_0x4aeb[22]] && new a[_0x4aeb[16]][_0x4aeb[22]]();
+        function h() {
+            k(), d[_0x4aeb[20]][_0x4aeb[23]](i);
+        }
+        function i() {
+            var b, c, g, h;
+            b = e[_0x4aeb[24]](d[_0x4aeb[20]]), c = l(), b >= c && (h = e[_0x4aeb[24]](d[_0x4aeb[20]]), 
+            f[_0x4aeb[25]](h) >= d[_0x4aeb[26]] ? (j(), g = o(function() {
+                m(b);
+            }), a(d[_0x4aeb[31]])[_0x4aeb[30]](d[_0x4aeb[29]])[_0x4aeb[28]]()[_0x4aeb[27]](g), 
+            g[_0x4aeb[32]]()) : m(b));
+        }
+        function j() {
+            d[_0x4aeb[20]][_0x4aeb[33]](_0x4aeb[23], i);
+        }
+        function k() {
+            a(d[_0x4aeb[35]])[_0x4aeb[34]]();
+        }
+        function l(b) {
+            var c, e;
+            return 0 === (c = a(d[_0x4aeb[31]])[_0x4aeb[30]](d[_0x4aeb[29]])[_0x4aeb[28]]())[_0x4aeb[36]]() ? 0 : (e = c[_0x4aeb[38]]()[_0x4aeb[37]] + c[_0x4aeb[39]](), 
+            b || (e += d[_0x4aeb[40]]), e);
+        }
+        function m(b, c) {
+            var e, g, i, l, m, o, p, q, r, s;
+            if (!(e = a(d[_0x4aeb[42]])[_0x4aeb[41]](_0x4aeb[6]))) return d[_0x4aeb[43]] && a(d[_0x4aeb[31]])[_0x4aeb[30]](d[_0x4aeb[29]])[_0x4aeb[28]]()[_0x4aeb[27]](d[_0x4aeb[43]]), 
+            j();
+            d[_0x4aeb[44]] && a[_0x4aeb[45]](d[_0x4aeb[44]]) && !1 === d[_0x4aeb[44]](b, e) || (f[_0x4aeb[46]](b, e), 
+            j(), g = n(), !1 !== d[_0x4aeb[47]] ? d[_0x4aeb[47]](g) : (a(d[_0x4aeb[31]])[_0x4aeb[30]](d[_0x4aeb[29]])[_0x4aeb[28]]()[_0x4aeb[27]](g), 
+            g[_0x4aeb[32]]()), i = e, l = function(b, f) {
+                !1 !== d[_0x4aeb[49]][_0x4aeb[48]](this, f) && (a(f)[_0x4aeb[34]](), a(d[_0x4aeb[31]])[_0x4aeb[30]](d[_0x4aeb[29]])[_0x4aeb[28]]()[_0x4aeb[27]](f), 
+                a(f)[_0x4aeb[32]]()), e = a(d[_0x4aeb[42]], b)[_0x4aeb[41]](_0x4aeb[6]), a(d[_0x4aeb[35]])[_0x4aeb[50]](a(d[_0x4aeb[35]], b)), 
+                n()[_0x4aeb[51]](), k(), e ? h() : j(), d[_0x4aeb[52]][_0x4aeb[48]](this, f), c && c[_0x4aeb[48]](this);
+            }, r = [], s = Date[_0x4aeb[15]](), m = m || d[_0x4aeb[53]], a[_0x4aeb[60]](i, null, function(b) {
+                0 === (o = a(d[_0x4aeb[31]], b)[_0x4aeb[55]](0))[_0x4aeb[54]] && (o = a(b)[_0x4aeb[56]](d[_0x4aeb[31]])[_0x4aeb[55]](0)), 
+                o && o[_0x4aeb[30]](d[_0x4aeb[29]])[_0x4aeb[58]](function() {
+                    r[_0x4aeb[57]](this);
+                }), l && (q = this, (p = Date[_0x4aeb[15]]() - s) < m ? setTimeout(function() {
+                    l[_0x4aeb[48]](q, b, r);
+                }, m - p) : l[_0x4aeb[48]](q, b, r));
+            }, _0x4aeb[59]));
+        }
+        function n() {
+            var b = a(_0x4aeb[61]);
+            return 0 === b[_0x4aeb[36]]() && (b = a(_0x4aeb[62] + d[_0x4aeb[63]] + _0x4aeb[64]))[_0x4aeb[34]](), 
+            b;
+        }
+        function o(b) {
+            var c = a(_0x4aeb[65]);
+            return 0 === c[_0x4aeb[36]]() && (c = a(_0x4aeb[66] + d[_0x4aeb[67]] + _0x4aeb[68]))[_0x4aeb[34]](), 
+            a(_0x4aeb[72], c)[_0x4aeb[71]](_0x4aeb[69])[_0x4aeb[70]](_0x4aeb[69], function() {
+                return o()[_0x4aeb[51]](), b[_0x4aeb[48]](), !1;
+            }), c;
+        }
+        f[_0x4aeb[75]](function(a, b, c) {
+            g && g[_0x4aeb[73]](a, c), d[_0x4aeb[74]][_0x4aeb[48]](this, a, c, b);
+        }), h(), g && g[_0x4aeb[76]]() && (j(), c = g[_0x4aeb[77]](), e[_0x4aeb[83]](function() {
+            var b;
+            c > 1 ? (function d(b) {
+                var c = l(!0);
+                c > 0 && m(c, function() {
+                    j(), f[_0x4aeb[25]](c) + 1 < b ? (d(b), a(_0x4aeb[80])[_0x4aeb[79]]({
+                        scrollTop: c
+                    }, 400, _0x4aeb[78])) : (a(_0x4aeb[80])[_0x4aeb[79]]({
+                        scrollTop: c
+                    }, 1e3, _0x4aeb[78]), h());
+                });
+            }(c), b = l(!0), a(_0x4aeb[82])[_0x4aeb[81]](b)) : h();
+        }));
+    }, a[_0x4aeb[16]][_0x4aeb[17]] = {
+        container: _0x4aeb[84],
+        scrollContainer: a(window),
+        item: _0x4aeb[85],
+        pagination: _0x4aeb[86],
+        next: _0x4aeb[87],
+        loader: _0x4aeb[88],
+        loaderDelay: 0,
+        triggerPageThreshold: 1,
+        trigger: _0x4aeb[89],
+        thresholdMargin: -100,
+        history: !0,
+        onPageChange: function() {},
+        beforePageChange: function() {},
+        onLoadItems: function() {},
+        onRenderComplete: function() {},
+        customLoaderProc: !1
+    }, a[_0x4aeb[16]][_0x4aeb[19]] = function() {
+        var b = !1, c = !1, d = this;
+        a(window)[_0x4aeb[90]](function() {
+            b = !0;
+        }), this[_0x4aeb[83]] = function(e) {
+            a(_0x4aeb[80])[_0x4aeb[81]](0), c || (b ? (e[_0x4aeb[48]](), c = !0) : setTimeout(function() {
+                d[_0x4aeb[83]](e);
+            }, 1));
+        }, this[_0x4aeb[24]] = function(a) {
+            return (a[_0x4aeb[60]](0) === window ? a[_0x4aeb[81]]() : a[_0x4aeb[38]]()[_0x4aeb[37]]) + a[_0x4aeb[39]]();
+        };
+    }, a[_0x4aeb[16]][_0x4aeb[21]] = function() {
+        var b = [ [ 0, document[_0x4aeb[7]].toString() ] ], c = function() {}, d = 1, e = new a[_0x4aeb[16]][_0x4aeb[19]]();
+        function f() {
+            var f, h, i, j, k;
+            h = g(f = e[_0x4aeb[24]](a(window))), i = function(a) {
+                for (var c = b[_0x4aeb[54]] - 1; c >= 0; c--) if (a > b[c][0]) return b[c];
+                return null;
+            }(f), d !== h && (j = i[0], k = i[1], c[_0x4aeb[48]]({}, h, j, k)), d = h;
+        }
+        function g(a) {
+            for (var c = b[_0x4aeb[54]] - 1; c > 0; c--) if (a > b[c][0]) return c + 1;
+            return 1;
+        }
+        a(window)[_0x4aeb[23]](f), this[_0x4aeb[25]] = function(b) {
+            return g(b = b || e[_0x4aeb[24]](a(window)));
+        }, this[_0x4aeb[75]] = function(a) {
+            c = a;
+        }, this[_0x4aeb[46]] = function(a, c) {
+            b[_0x4aeb[57]]([ a, c ]);
+        };
+    }, a[_0x4aeb[16]][_0x4aeb[22]] = function() {
+        var a = !1, b = !1;
+        b = !!(window[_0x4aeb[22]] && history[_0x4aeb[91]] && history[_0x4aeb[92]]), b = !1, 
+        this[_0x4aeb[73]] = function(a, b) {
+            this[_0x4aeb[94]]({
+                page: a
+            }, _0x4aeb[93], b);
+        }, this[_0x4aeb[76]] = function() {
+            return !1 !== this[_0x4aeb[95]]();
+        }, this[_0x4aeb[77]] = function() {
+            return this[_0x4aeb[76]]() ? this[_0x4aeb[95]]()[_0x4aeb[96]] : 1;
+        }, this[_0x4aeb[95]] = function() {
+            var a;
+            if (b) {
+                if ((a = history[_0x4aeb[97]]) && a[_0x4aeb[16]]) return a[_0x4aeb[16]];
+            } else if (_0x4aeb[98] === window[_0x4aeb[7]][_0x4aeb[100]][_0x4aeb[99]](0, 7)) return {
+                page: parseInt(window[_0x4aeb[7]][_0x4aeb[100]][_0x4aeb[101]](_0x4aeb[98], _0x4aeb[93]), 10)
+            };
+            return !1;
+        }, this[_0x4aeb[94]] = function(b, c, d) {
+            a ? this[_0x4aeb[92]](b, c, d) : this[_0x4aeb[91]](b, c, d);
+        }, this[_0x4aeb[91]] = function(c, d, e) {
+            var f;
+            b ? history[_0x4aeb[91]]({
+                ias: c
+            }, d, e) : (f = c[_0x4aeb[96]] > 0 ? _0x4aeb[98] + c[_0x4aeb[96]] : _0x4aeb[93], 
+            window[_0x4aeb[7]][_0x4aeb[100]] = f), a = !0;
+        }, this[_0x4aeb[92]] = function(a, c, d) {
+            b ? history[_0x4aeb[92]]({
+                ias: a
+            }, c, d) : this[_0x4aeb[91]](a, c, d);
+        };
+    };
+}(jQuery);
