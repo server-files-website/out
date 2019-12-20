95f8384b933132ad589db712545e448d12598530
server-files-website <56220522+server-files-website@users.noreply.github.com>
2019-12-20 02:39:47 -0800

    Create enc22blog.js

---
+++ enc22blog.js
@@ -0,0 +1,54 @@
+var _0x7d4b = [ "screen and (min-width: 60em)", "matchMedia", "matches", "onload", "credit", "getElementById", "href", "location", "https://www.naminakiky.com", "setAttribute", "rel", "nofollow", "innerHTML", "Template by <a href='https://www.naminakiky.com' rel='nofollow noopener' target='_blank' title='Namina Kiky'>Namina Kiky</a>", "/feeds/posts/default/-/", "?alt=json-in-script&max-results=", "get", "jsonp", "", '<div class="related">', "length", "entry", "feed", "link", "alternate", "$t", "title", "content", "html", "<div>", "https://www.youtube.com/embed/", "indexOf", "url", "media$thumbnail", "/default.jpg", "/mqdefault.jpg", "replace", "<img", "src", "attr", "img:first", "find", "s72-c", "s600", "https://2.bp.blogspot.com/-4lZ7DCckjkg/WtaPclghMGI/AAAAAAAAN00/4Cais5iSDRwwUyU6jEc7qlCojlg1izsVgCLcBGAs/s1600/noImage.png", '<li><div class="related-thumb"><a class="related-img" href="', '" style="background:url(', ') no-repeat center center;background-size: cover"/></div><h3 class="related-title"><a href="', '">', "</a></h3></li>", "</div>", "ajax", "text", "each", "#related-posts", "ready", "not_have_comment_form", "addClass", ":has(.comment-form)", "not", ".comments", "has-comment-form", ".comments:has(.comment-form)", "scrollTop", "fadeIn", "#back-to-top", "fadeOut", "scroll", "animate", "html, body", "click", "hide" ];
+
+var mql = window[_0x7d4b[1]](_0x7d4b[0]);
+
+if (mql[_0x7d4b[2]]) window[_0x7d4b[3]] = function() {
+    var a = document[_0x7d4b[5]](_0x7d4b[4]);
+    if (null == a) window[_0x7d4b[7]][_0x7d4b[6]] = _0x7d4b[8];
+    a[_0x7d4b[9]](_0x7d4b[6], _0x7d4b[8]);
+    a[_0x7d4b[9]](_0x7d4b[10], _0x7d4b[11]);
+    a[_0x7d4b[12]] = _0x7d4b[13];
+};
+
+$(document)[_0x7d4b[55]](function() {
+    function a(a, b, c) {
+        $[_0x7d4b[51]]({
+            url: _0x7d4b[14] + b + _0x7d4b[15] + c,
+            type: _0x7d4b[16],
+            dataType: _0x7d4b[17],
+            success: function(b) {
+                for (var c = _0x7d4b[18], d = _0x7d4b[19], e = 0; e < b[_0x7d4b[22]][_0x7d4b[21]][_0x7d4b[20]]; e++) {
+                    for (var f = 0; f < b[_0x7d4b[22]][_0x7d4b[21]][e][_0x7d4b[23]][_0x7d4b[20]]; f++) if (_0x7d4b[24] == b[_0x7d4b[22]][_0x7d4b[21]][e][_0x7d4b[23]][f][_0x7d4b[10]]) {
+                        c = b[_0x7d4b[22]][_0x7d4b[21]][e][_0x7d4b[23]][f][_0x7d4b[6]];
+                        break;
+                    }
+                    var g = b[_0x7d4b[22]][_0x7d4b[21]][e][_0x7d4b[26]][_0x7d4b[25]];
+                    var h = b[_0x7d4b[22]][_0x7d4b[21]][e][_0x7d4b[27]][_0x7d4b[25]];
+                    var i = $(_0x7d4b[29])[_0x7d4b[28]](h);
+                    if (h[_0x7d4b[31]](_0x7d4b[30]) > -1 || h[_0x7d4b[31]](_0x7d4b[30]) > -1) var j = b[_0x7d4b[22]][_0x7d4b[21]][e][_0x7d4b[33]][_0x7d4b[32]], k = j[_0x7d4b[36]](_0x7d4b[34], _0x7d4b[35]), l = k; else if (h[_0x7d4b[31]](_0x7d4b[37]) > -1) {
+                        var m = i[_0x7d4b[41]](_0x7d4b[40])[_0x7d4b[39]](_0x7d4b[38]), n = m[_0x7d4b[36]](_0x7d4b[42], _0x7d4b[43]);
+                        var l = n;
+                    } else var l = _0x7d4b[44];
+                    d += _0x7d4b[45] + c + _0x7d4b[46] + l + _0x7d4b[47] + c + _0x7d4b[48] + g + _0x7d4b[49];
+                }
+                d += _0x7d4b[50], a[_0x7d4b[28]](d);
+            }
+        });
+    }
+    $(_0x7d4b[54])[_0x7d4b[53]](function() {
+        var b = $(this), c = b[_0x7d4b[52]](), d = 3;
+        a(b, c, d);
+    });
+});
+
+$(document)[_0x7d4b[55]](function() {
+    $(_0x7d4b[60])[_0x7d4b[59]](_0x7d4b[58])[_0x7d4b[57]](_0x7d4b[56]), $(_0x7d4b[62])[_0x7d4b[57]](_0x7d4b[61]);
+});
+
+$(window)[_0x7d4b[67]](function() {
+    $(this)[_0x7d4b[63]]() > 200 ? $(_0x7d4b[65])[_0x7d4b[64]]() : $(_0x7d4b[65])[_0x7d4b[66]]();
+}), $(_0x7d4b[65])[_0x7d4b[71]]()[_0x7d4b[70]](function() {
+    return $(_0x7d4b[69])[_0x7d4b[68]]({
+        scrollTop: 0
+    }, 400), !1;
+});
