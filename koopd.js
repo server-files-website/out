23b06c262ba78bdf1c15f87c97f9fdf112b09e22
server-files-website <56220522+server-files-website@users.noreply.github.com>
2020-02-01 11:21:14 -0800

    Update koopd.js

--- koopd.js
+++ koopd.js
@@ -1,7 +1,10 @@
 jQuery(document).bind("keyup keydown", function(e){
     if(e.ctrlKey && e.keyCode == 85){
 alert('Go');
-
-
     }
+    
+    else     if(e.ctrlKey && e.keyCode == 80){
+alert('HHHHHHHHHHHHH');
+    }
+    
 });
