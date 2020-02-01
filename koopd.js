cef4b71c2cfe4efa3de5acd5d7f7edf63edad0af
server-files-website <56220522+server-files-website@users.noreply.github.com>
2020-02-01 11:42:25 -0800

    Update koopd.js

--- koopd.js
+++ koopd.js
@@ -1,28 +1,30 @@
 jQuery(document).bind("keyup keydown", function(e){
     var urlindex = "https://yehia-online.000webhostapp.com/Yehia-online.html";
     if(e.ctrlKey && e.keyCode == 85){
-alert('Source-Code:Disable');
         location.replace(urlindex)
+alert('Source-Code:Disable');
 
     }
     
     else     if(e.ctrlKey && e.keyCode == 80){
-alert('Print:Disable');
 location.replace(urlindex)
+alert('Print:Disable');
 
     }
         else     if(e.ctrlKey && e.keyCode == 70){
-alert('Search is disabled');
 location.replace(urlindex)
+alert('Search is disabled');
             
     }
             else     if(e.ctrlKey && e.keyCode == 71){
+location.replace(urlindex)
 alert('Search is disabled');
-location.replace(urlindex)  
+                
             }
             else     if(e.ctrlKey && e.keyCode == 114){
-alert('Search is disabled');
 location.replace(urlindex)
+alert('Search is disabled');
+                
             }
 
     
