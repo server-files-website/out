ec84a2a78794f419dc07a9f0cad47abc203da404
server-files-website <56220522+server-files-website@users.noreply.github.com>
2020-02-01 11:54:27 -0800

    Update koopd.js

--- koopd.js
+++ koopd.js
@@ -30,48 +30,4 @@ alert('Search is disabled');
     
 });
 
-document.onkeyup = function (Yehia_F_key) {
-  if (Yehia_F_key.which == 83) {
-$(document).ready(function() {
-    setTimeout(function() {
-        $(#search_auto)[0].click();
-    },10);
-});  }
-  
-   else if (Yehia_F_key.which == 68) {
-$(document).ready(function() {
-    setTimeout(function() {
-        $(.dntoggle)[0].click();
-    },10);
-});  } 
-   else if (Yehia_F_key.which == 84) {
-$(document).ready(function() {
-    setTimeout(function() {
-        $(.img)[0].click();
-    },10);
-});  } 
-  
-     else if (Yehia_F_key.which == 77) {
-$(document).ready(function() {
-    setTimeout(function() {
-        $(#yehia_morep)[0].click();
-    },10);
-});  } 
 
-
-
-
-
-     else if (Yehia_F_key.which == 89) {
-
-profile_u = https://www.yehia.online/p/profile.html;
-window.open(profile_u);
-
- } 
-
-  
-
-
-
-
-};
