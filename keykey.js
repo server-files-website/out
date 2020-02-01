c45b628b8289bbc33ae067ab26bb2caac23064f8
server-files-website <56220522+server-files-website@users.noreply.github.com>
2020-02-01 07:54:44 -0800

    Create keykey.js

---
+++ keykey.js
@@ -0,0 +1,31 @@
+document.onkeyup = function (yehia_F_Key) {
+  if (yehia_F_Key.which == 77) {
+$("document").ready(function() {
+    setTimeout(function() {
+        $("#yehia_morep")[0].click();
+    },10);
+});
+  } 
+  
+  else if (e.ctrlKey && e.which == 66) {
+    alert("Ctrl + B shortcut combination was pressed");
+  } 
+  
+  
+    else if (e.ctrlKey && e.which == 66) {
+    alert("Ctrl + B shortcut combination was pressed");
+  } 
+    else if (e.ctrlKey && e.which == 66) {
+    alert("Ctrl + B shortcut combination was pressed");
+  } 
+    else if (e.ctrlKey && e.which == 66) {
+    alert("Ctrl + B shortcut combination was pressed");
+  } 
+    else if (e.ctrlKey && e.which == 66) {
+    alert("Ctrl + B shortcut combination was pressed");
+  } 
+    else if (e.ctrlKey && e.which == 66) {
+    alert("Ctrl + B shortcut combination was pressed");
+  } 
+  
+};
