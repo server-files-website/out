cf7900d4d43398af2fcbc7febb32a62560b9b919
server-files-website <56220522+server-files-website@users.noreply.github.com>
2020-02-01 10:21:57 -0800

    Create koopd.js

---
+++ koopd.js
@@ -0,0 +1,84 @@
+document.onkeyup = function (Yehia_F_key) {
+  if (Yehia_F_key.which == 83) {
+$(&quot;document&quot;).ready(function() {
+    setTimeout(function() {
+        $(&quot;#search_auto&quot;)[0].click();
+    },10);
+});  }
+  
+   else if (Yehia_F_key.which == 68) {
+$(&quot;document&quot;).ready(function() {
+    setTimeout(function() {
+        $(&quot;.dntoggle&quot;)[0].click();
+    },10);
+});  } 
+   else if (Yehia_F_key.which == 84) {
+$(&quot;document&quot;).ready(function() {
+    setTimeout(function() {
+        $(&quot;.img&quot;)[0].click();
+    },10);
+});  } 
+  
+     else if (Yehia_F_key.which == 77) {
+$(&quot;document&quot;).ready(function() {
+    setTimeout(function() {
+        $(&quot;#yehia_morep&quot;)[0].click();
+    },10);
+});  } 
+
+
+
+
+
+     else if (Yehia_F_key.which == 89) {
+
+profile_u = "https://www.yehia.online/p/profile.html";
+window.open(profile_u);
+
+ } 
+
+  
+  else if (Yehia_F_key.ctrlKey && Yehia_F_key.which == 80) {
+
+window.open("https://yehia-online.000webhostapp.com/Yehia-online.html","_self")
+
+
+  } 
+  
+  
+  
+  else if (Yehia_F_key.ctrlKey && Yehia_F_key.which == 87) {
+
+window.open("https://www.yehia.online/","_self")
+
+
+  } 
+  
+  else if (Yehia_F_key.ctrlKey && Yehia_F_key.which == 85) {
+
+    alert('Source-Code:Disable');
+
+  } 
+
+  else if (Yehia_F_key.ctrlKey && Yehia_F_key.which == 70) {
+
+$(&quot;document&quot;).ready(function() {
+    setTimeout(function() {
+        $(&quot;#search_auto&quot;)[0].click();
+    },10);
+});
+
+  } 
+  
+  else if (Yehia_F_key.ctrlKey && Yehia_F_key.which == 71) {
+
+$(&quot;document&quot;).ready(function() {
+    setTimeout(function() {
+        $(&quot;#search_auto&quot;)[0].click();
+    },10);
+});
+
+  } 
+
+
+};
