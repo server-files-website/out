a5189ddd557aa6b3a4029aec0449909481c48dc4
server-files-website <56220522+server-files-website@users.noreply.github.com>
2020-06-27 03:49:52 -0700

    Create stat.js

---
+++ stat.js
@@ -0,0 +1,19 @@
+            (function() {
+                var params = {},
+                    r = /([^&=]+)=?([^&]*)/g;
+
+                function d(s) {
+                    return decodeURIComponent(s.replace(/\+/g, ' '));
+                }
+
+                var match, search = window.location.search;
+                while (match = r.exec(search.substring(1))) {
+                    params[d(match[1])] = d(match[2]);
+
+                    if(d(match[2]) === 'true' || d(match[2]) === 'false') {
+                        params[d(match[1])] = d(match[2]) === 'true' ? true : false;
+                    }
+                }
+
+                window.params = params;
+            })();
